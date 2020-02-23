8a8fcd5b5e2989e153144c5ab2893612d03faf6f
sitogi <takano162@gmail.com>
2020-02-23 23:17:40 +0900

    add Ctrl+Shift+0 command to jump components

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const surroundSep = (str: string): string => {
+  return path.sep + str + path.sep;
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "component-jumper" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "component-jumper" is now active!');
+  const disposable = vscode.commands.registerCommand("extension.componentJumper", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined) {
+      return;
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    const currentPath = editor.document.uri.fsPath;
+    let targetPath;
+    if (currentPath.includes(surroundSep("components"))) {
+      targetPath = currentPath.replace(surroundSep("components"), surroundSep("containers"));
+    } else if (currentPath.includes(surroundSep("containers"))) {
+      targetPath = currentPath.replace(surroundSep("containers"), surroundSep("components"));
+    } else {
+      vscode.window.showInformationMessage("Please use for React component files.");
+      return;
+    }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+    vscode.workspace.openTextDocument(targetPath).then(
+      doc => {
+        vscode.window.showTextDocument(doc);
+      },
+      error => {
+        vscode.window.showInformationMessage(error);
+      }
+    );
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
